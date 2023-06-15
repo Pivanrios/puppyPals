b5e4e6c2e21ef3a1b8cc263eafae67234db5d7ed
Pedro Perez <pipr96@gmail.com>
2023-06-14 20:44:55 -0700

    render puppy details

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,31 @@ import {useState} from 'react'
 
 
 function App() {
-  console.log("imported data:",puppyList)
+  console.log("imported data:",puppyList);
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);{//add functionality 
+  }
+  const featuredPup = puppies.find((pup)=> pup.id ===featPupId);
+  console.log("featuredpup:", featuredPup);
+                                                
+
   
   return (
     <>
     {//Refactor the Starting Point
     } <div>
       {puppyList.map((puppy =>{
-        return <p key={puppy.id}>{puppy.name}</p>
+        return <p onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
       }))}
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email:{featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
       </div>
     </>
   )

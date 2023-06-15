7c4bb9413ee85bcd1ca73a016247295b5cd4676f
Pedro Perez <pipr96@gmail.com>
2023-06-14 20:00:22 -0700

    render list of puppy names

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import './App.css'
+import {puppyList} from './data.js'
+import {useState} from 'react'
+
+
 
 function App() {
+  console.log("imported data:",puppyList)
+  const [puppies, setPuppies] = useState(puppyList);
+  
   return (
     <>
     {//Refactor the Starting Point
     } <div>
-
+      {puppyList.map((puppy =>{
+        return <p key={puppy.id}>{puppy.name}</p>
+      }))}
       </div>
     </>
   )
